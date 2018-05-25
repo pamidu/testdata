0b869b32fdf7ff3631239898363e7c2470f4ecd3
Pamidu jayaranga <pamidu@users.noreply.github.com>
2018-05-25 14:55:58 +0530

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,11 +1,16 @@
 module.exports = {
   "DB": {
-    "Type":"postgres",
-    "User":"duo",
-    "Password":"DuoS123",
-    "Port":5432,
-    "Host":"localhost",
-    "Database":"duo"
+     "Type": "postgres",
+      "User": "duo",
+      "Password": "DuoS123",
+      "Port": 5432,
+      "Host": "127.0.0.1",
+      "Database": "facetone",
+      "Cluster": true,
+      "dialectOptions":{
+          "keepAlive": false,
+          "statement_timeout": 10000
+      }
   },
 
   // "Redis":
